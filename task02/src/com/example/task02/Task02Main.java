803f40581dabbda381541fdf692c8709d9d4c58a
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-11-15 02:07:39 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,20 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        int prevByte = System.in.read();
+        int nextByte;
+        while (prevByte != -1){
+            nextByte = System.in.read();
+            if (prevByte != 13 || nextByte != 10){
+                System.out.write(prevByte);
+            }
+            prevByte = nextByte;
+        }
+        System.out.flush();
     }
 }
